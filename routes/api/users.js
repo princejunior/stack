e5c39848e80373fa0d7ac992e84634a2757b9d0c
Elijah Elliott <elijahelliott54@gmail.com>
2020-03-02 11:19:57 -0800

    edited

---
+++ routes/api/users.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
+
+// up two levels and to User.js
+const User = require('../../models/User');
+
+// @route   POST api/users
+// @desc    Register User
+// @access  Public
+// router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valide email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    // console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // see if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+      }
+      //Get users gravatar
+      const avatar = gravatar.url(email, {
+        size: '200',
+        rating: 'pg',
+        default: 'mm'
+      });
+
+      user = new User({ name, email, avatar, password });
+
+      // password goes through 10 rounds
+      // the high the number the slower it will get but more secured
+      const salt = await bcrypt.genSalt(10);
+
+      // creates a hash
+      //Encrypt password
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      //Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+      res.send('User registered');
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server Error');
+    }
+
+    res.send('User route');
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+// connect database
+connectDB();
+// Init MiddleWare
+app.use(
+  express.json({
+    extended: false
+  })
+);
 app.get('/', (request, response) => response.send('API running'));
+
+//Defines routes
+app.use('/api/users', require('./routes/api/users.js'));
+app.use('/api/posts', require('./routes/api/posts.js'));
+app.use('/api/auth', require('./routes/api/auth.js'));
+app.use('/api/profile', require('./routes/api/profile.js'));
+
 const port = process.env.port || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
